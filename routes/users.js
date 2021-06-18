a79ceca93e52cf6f084e6d7ca099263ae9633ca0
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2021-06-17 18:54:31 -0600

    staring point User API http methods

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+ 
+router.post('/', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+ 
+router.put('/', (req, res) => {
+  return res.send('PUT HTTP method on user resource');
+});
+ 
+router.delete('/', (req, res) => {
+  return res.send('DELETE HTTP method on user resource');
+});
+
 module.exports = router;
